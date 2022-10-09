7f2af6961f3dd5c7c03f6dba971fe0e8146a23dc
nileshsaple-java <68497115+nileshsaple-java@users.noreply.github.com>
2022-10-09 12:59:47 +0530

    Update JDBC.java

--- src/JDBC.java
+++ src/JDBC.java
@@ -2,23 +2,33 @@ import java.sql.*;
 
 public class JDBC {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
         String url = "jdbc:mysql://localhost:3306/mysql80";
         String user = "root";
         String password = "root";
 
+        String sql = "SELECT * from employees";
         Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, user,password);
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
 
+        /**
+         * employeeId int PK
+         * lastName varchar(50)
+         * firstName varchar(50)
+         * email varchar(100)
+         * jobTitle varchar(50)
+         */
+        while(rs.next()){
+            System.out.println(rs.getInt("employeeId"));
+            System.out.println(rs.getString("lastName"));
+            System.out.println(rs.getString("firstName"));
+            System.out.println(rs.getString("email"));
+            System.out.println(rs.getString("jobTitle"));
 
-        // create a connection to the database
-        Connection conn = DriverManager.getConnection(url, user, password);
+        }
 
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from employees");
-        while (rs.next())
-            System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
-        conn.close();
-
-        System.out.println(conn);
-
+        con.close();
     }
 }
